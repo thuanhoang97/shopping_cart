86f34b21473e85a8fa5178f1a5748688e45ec95b
ThuanHoang <hoangvietthuan97@gmail.com>
2018-04-28 09:36:54 +0700

    upload new html

--- server.js
+++ server.js
@@ -2,107 +2,122 @@ var express= require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var session = require('express-session');
-var mysqldb = require('./mysql-database').connectDatabase();
+var mysqldb = require('./mysql-database');
 
-// mysqldb.connectDatabase(function(err, db){
-// 	if(err){
-// 		console.log(err);
-// 		process.exit();
-// 	}
+mysqldb.initDatabase(function(err, db){
+	if(err){
+		console.log(err);
+		process.exit();
+	}
+	else{
+		var app = express();
 
-	
-// });
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded());
 
+		app.use(session({
+		secret: 'js'
+		}));
+		app.use(function(req, res, next){
+		// console.log('session user: ', req.session.user);
+		console.log('method: ',req.method, 'url: ', req.url);
+		// console.log(mysqldb);
+		next();
+		});
 
-var app = express();
+		app.use(express.static(path.join(__dirname, 'static/html')));
+		// app.use('/login-register', express.static(path.join(__dirname, 'static/html/register')))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+		app.use('/css', express.static(path.join(__dirname,'static/css')));
+		app.use('/img', express.static(path.join(__dirname, 'static/img')));
+		app.use('/js', express.static(path.join(__dirname, 'static/js')));
+		app.use('/video', express.static(path.join(__dirname, 'static/video')));
+		// app.use('');
 
-app.use(session({
-	secret: 'js'
-}));
-app.use(function(req, res, next){
-	// console.log('session user: ', req.session.user);
-	console.log('method: ',req.method, 'url: ', req.url);
-	// console.log(mysqldb);
-	next();
-});
+		// app.use('/login', express.static(path.join(__dirname, 'client/login')));
+		// app.use('/register', express.static(path.join(__dirname, 'client/register')));
+		// app.use('/css', express.static(path.join(__dirname, 'client/css')));
+		// app.use('/js', express.static(path.join(__dirname, 'client/js')));
+		// app.use('/img', express.static(path.join(__dirname, 'client/img')));
+		// app.get('/register', function(res, rep){
 
-app.use(express.static(path.join(__dirname, 'client/home')));
+		// });.
+		app.get('/login-register', function(req, res){
+			res.sendFile(path.join(__dirname, 'static/html/login-register.html'));
+		});
 
-app.use('/login', express.static(path.join(__dirname, 'client/login')));
-app.use('/register', express.static(path.join(__dirname, 'client/register')));
-app.use('/css', express.static(path.join(__dirname, 'client/css')));
-app.use('/js', express.static(path.join(__dirname, 'client/js')));
-app.use('/img', express.static(path.join(__dirname, 'client/img')));
+		app.post('/login', function(req, res){
+			var email = req.body.email;
+			var password = req.body.password;
+			console.log('login request:\n username:',email,' password:',password );
 
-
-app.post('/login', function(req, res){
-	var username = req.body.username;
-	var password = req.body.password;
-	console.log('login request:\n username:',username,' password:',password );
-	if(username && password){
-		var LOGIN= 'SELECT * FROM shoopingdb.usertb '  
-			+'WHERE username="'+username+'" and password="'+password+'"';
-		db.query(LOGIN, function(err, result){
-			if(err){
-				console.log(err);
-			}
-			else{
-				if(result.length === 1) {
-						//var resultsk = JSON.stringify(result);
-						req.session.userId = result[0].id;
-						req.session.username = result[0].username;
-						console.log((result));
-						console.log('Login sucessfullt with ' + username);
-						res.redirect('/');
-				}
-				else{		
-						console.log('Can not login');
-						res.redirect('/');		
-					}
-				}
-			});
-		}
-	});
-
-app.post('/register', function(req, res){
-	console.log(req.body);
-	var email = req.body.email;
-	var password = req.body.password;
-	var comfirmPassword = req.body.comfirmPassword;
-	var phone = req.body.phone;
-	var name = req.body.name;
-
-
-
-	var REGISTER = 'INSERT INTO TABLE shoppingdb.user(id, email, password)'
-				+ 'VALUES(NULL,' + email + ',' + password + ')';
-
-	mysqldb.query(REGISTER, function(err, result){
-		if(err){
-			console.log(err);
-		}
-		else{
-			var GET_ID_USER = 'SELECT id FROM shoppingdb.user WHERE email = ' + email;
-			mysqldb.query(GET_ID_USER, function(err, result){
+			if(email && password){
+			var LOGIN= 'SELECT * FROM shoppingdb.user '  
+				+'WHERE email="'+email+'" and password="'+password+'"';
+			db.query(LOGIN, function(err, result){
 				if(err){
 					console.log(err);
+					res.redirect('/login-register');	
 				}
 				else{
-					console.log(result);
+					if(result.length === 1) {
+							//var resultsk = JSON.stringify(result);
+							req.session.userId = result[0].id;
+							req.session.email = result[0].email;
+							console.log((result));
+							console.log('Login sucessfully with ' + email);
+							
+					}
+					else{		
+							console.log('Can not login');	
+						}
+					}
+					res.redirect('/');
+				});
+			}
+		});
+
+		app.post('/register', function(req, res){
+			console.log(req.body);
+			var username = req.body.username;
+			var phone = req.body.phone;
+			var email = req.body.email;
+			var password = req.body.password;
+			var comfirmPassword = req.body.comfirmPassword;
+			
+
+			var REGISTER = 'INSERT INTO shoppingdb.user(id, email, password)'
+						+ ' VALUES(NULL, "' + email + '", "' + password + '")';
+
+			db.query(REGISTER, function(err, result){
+				if(err){
+					console.log(err);
+					res.redirect('/login-register');
 				}
-			})
+				else
+				{
+					var GET_ID_USER = '(SELECT id FROM shoppingdb.user WHERE email = "' + email+'")';
+					var ADD_USER_INFO = 'INSERT INTO shoppingdb.userinfo(id, username, phone) VALUES'
+						+ '('+GET_ID_USER+', "'+ username + '", "' + phone + '")'
+					
+					db.query(ADD_USER_INFO, function(err, result){
+						if(err){
+							console.log(err);
+							res.redirect('/login-register');
+						}
+						else{
+							res.redirect('/');
+						}
+					})
+				}
+			});
+		});
 
-			// var ADD_USER_INFO = 'INSERT INTO TABLE shoppingdb.userinfo(id, username, email, phone) VALUES('
-			// 	+'(SELECT id from shoppingdb.user WHERE id = ' + 
-		}
-	});
 
+		app.listen(1997, function(){
+		console.log('Server is running on port 1997...');
+		});
+}
 });
 
 
-app.listen(1997, function(){
-	console.log('Server is running on port 1997...');
-});
